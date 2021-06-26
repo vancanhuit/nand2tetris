388eb88c225273dbdbe3163689a1cdc256e680c3
vancanhuit <vancanhuit@gmail.com>
2021-06-26 21:17:56 +0700

    Chapter 04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,61 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+    @SCREEN // A = SCREEN
+    D=A // D = SCREEN
+    @addr // A = addr
+    M=D // RAM[addr] = SCREEN
+
+    @i // A = i
+    M=0 // RAM[i] = 0
+
+    @8192 // A = 8192
+    D=A // D = 8192
+    @R0 // A = 0
+    M=D // RAM[0] = 8192
+    
+    @R1 // A = 1
+    M=-1 // RAM[1] = -1
+    
+    @KBD // A = KBD
+    D=M // D = RAM[KBD]
+    @UPDATE
+    D;JNE
+    
+    @R1 // A = 1
+    M=0 // RAM[1] = 0
+
+    @KBD // A = KBD
+    D=M // D = RAM[KBD]
+    @UPDATE
+    0;JMP
+    
+(UPDATE)
+    @R0 // A = 0
+    D=M // D = RAM[0] = 8192
+    @i // A = i
+    D=M-D // D = RAM[i] - 8192
+    @LOOP
+    D;JGE
+    
+    @i // A = i
+    D=M // D = RAM[i]
+    @addr // A = addr
+    D=M+D // D = RAM[addr] + RAM[i]
+    @R2 // A = 2
+    M=D // RAM[2] = RAM[addr] + RAM[i]
+    
+    @R1 // A = 1
+    D=M // D = RAM[1]
+    
+    @R2 // A = 2
+    A=M // A = RAM[2]
+    M=D // RAM[RAM[2]] = RAM[RAM[addr] + RAM[i]] = D = RAM[color]
+    
+    @i // A = i
+    M=M+1 // RAM[i] = RAM[i] + 1
+    @UPDATE
+    0;JMP
+
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,53 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// RAM[p] = 0
+@p // A = p
+M=0 // RAM[p] = 0
+
+// RAM[0] == 0 goto STOP
+@R0 // A = 0
+D=M // D = RAM[0]
+@STOP
+D;JEQ
+
+// RAM[1] == 0 goto STOP
+@R1 // A = 1
+D=M // D = RAM[1]
+@STOP
+D;JEQ
+
+// RAM[i] = 1
+@i
+M=1
+
+(LOOP)
+    @R1 // A = 1
+    D=M // D = RAM[1]
+    
+    // RAM[i] > RAM[1] goto END
+    @i // A = i
+    D=D-M // D = RAM[1] - RAM[i]
+    @STOP
+    D;JLT
+    
+    @R0 // A = 0
+    D=M // D = RAM[0]
+    @p // A = p
+    M=M+D // RAM[p] = RAM[p] + RAM[0]
+    @i // A = i
+    M=M+1 // RAM[i] = RAM[i] + 1
+    @LOOP
+    0;JMP
+(STOP)
+    // RAM[2] = RAM[p]
+    @p // A = p
+    D=M // D = RAM[p]
+    @R2 // A = 2
+    M=D // RAM[2] = D = RAM[p]
+    @END
+    0;JMP
+(END)
+    @END
+    0;JMP
